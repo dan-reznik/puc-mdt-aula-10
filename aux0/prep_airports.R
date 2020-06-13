f7fd3a0168c9b4a0a27967f1dbd93b42b3b4f718
ivertonN <44214892+ivertonN@users.noreply.github.com>
2020-06-13 11:23:42 -0300

    reairports: dução de complexidade

--- aux0/prep_airports.R
+++ aux0/prep_airports.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(tidygraph)
 library(igraph)
-
+library(geosphere)
 
 # Flights / map geosphere
 df_airports <- read_csv("data_sb/Dataset3-Airlines-NODES.csv")
@@ -37,6 +37,37 @@ df_flights_grouped <- df_flights %>%
   rename(group_id_1 = group) %>%
   left_join(df_airports_grouped %>% select(ID, group), by = c("Target" = "ID")) %>%
   rename(group_id_2 = group)
+
+##criando curved lines
+
+curved_lines <- list()
+curved_lines_color_ind <- c()
+for(i in 1:nrow(df_flights_grouped))  {
+  #Origem
+  node1 <- df_airports_grouped %>% 
+    filter(df_airports_grouped$ID == df_flights_grouped[i,]$Source)
+  
+  #destino
+  node2 <- df_airports_grouped %>%
+    filter(df_airports_grouped$ID == df_flights_grouped[i,]$Target)
+  
+  #numero da cor na paleta
+  curved_lines_color_ind[i] = round(100*df_flights_grouped[i,]$Freq / max(df_flights_grouped$Freq))
+  
+  # Cria linhas curvas
+  curved_lines[i] <-
+    gcIntermediate(
+      p1 = c(node1$longitude, node1$latitude)
+      , p2 = c(node2$longitude, node2$latitude)
+      , breakAtDateLine = TRUE
+      , n = 12
+      , addStartEnd = TRUE
+      , sp = TRUE
+    )
+}
+df_flights_grouped <- df_flights_grouped %>%
+  mutate(curved_line = curved_lines,
+         curved_line_color_ind = curved_lines_color_ind)
 
 df_flights_grouped %>%
   write_rds("data_sb/df_flights_grouped.rds")
